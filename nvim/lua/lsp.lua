b07262e1ae9781ecb06f54c97eaba0cd85e8818e
Ahn Shinho <fracsinus@gmail.com>
2023-08-30 13:04:47 +0900

    nvim(lsp): Add lua server

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -149,6 +149,30 @@ require("lspconfig").pyright.setup({
   end,
 })
 
+require("lspconfig").lua_ls.setup({
+  single_file_support = true,
+  on_init = function(client)
+    local path = client.workspace_folders[1].name
+    if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
+      client.config.settings = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+        runtime = {
+          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+          version = 'LuaJIT'
+        },
+        -- Make the server aware of Neovim runtime files
+        workspace = {
+          library = { vim.env.VIMRUNTIME }
+          -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+          -- library = vim.api.nvim_get_runtime_file("", true)
+        }
+      })
+
+      client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+    end
+    return true
+  end,
+})
+
 require("lsp_signature").setup({})
 
 vim.diagnostic.config({
