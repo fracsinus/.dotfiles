9ef510c9c5eb4f56bc0d6b01928ddd9100cd5f78
Ahn Shinho <fracsinus@gmail.com>
2023-10-20 23:20:58 +0900

    nvim: Break down LSP settings

---
+++ nvim/lua/lsp/pyright.lua
@@ -0,0 +1,40 @@
+local base = require("lsp._base")
+
+local function get_python_path()
+  if vim.env.VIRTUAL_ENV then
+    return util.path.join(vim.env.VIRTUAL_ENV, "bin", "python")
+  end
+
+  return vim.fn.exepath("python")
+end
+
+require("lspconfig").pyright.setup({
+  single_file_support = true,
+  settings = {
+    analysis = {
+      diagnosticMode = "openFilesOnly",
+    }
+  },
+  on_attach = base.on_attach_lsp,
+  capabilities = base.capabilities,
+  before_init = function(_, config)
+    config.settings.python.pythonPath = get_python_path()
+  end,
+})
+
+-- local M = {
+--   _diagnostic_options = {
+--     virtual_text = true,
+--   },
+-- }
+
+-- M.toggle_virtual_text = function()
+--   M._diagnostic_options.virtual_text = not M._diagnostic_options.virtual_text
+--   -- print(type(vim.lsp.diagnostic.on_publish_diagnostics))
+--   vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+--     vim.lsp.diagnostic.on_publish_diagnostics,
+--     M._diagnostic_options
+--   )
+-- end
+
+-- return M
---
+++ nvim/lua/lsp/trouble.lua
@@ -0,0 +1,18 @@
+-- diagnostics list
+--
+require("packer").use("folke/trouble.nvim")
+require("trouble").setup({ mode = "document_diagnostics", use_diagnostic_sign = true })
+
+vim.api.nvim_set_keymap("n", "<C-Q>", ":TroubleToggle<CR>", { noremap = true })
+
+local virtual_text = true
+vim.keymap.set(
+  "n",
+  "+",
+  function()
+    virtual_text = not virtual_text
+    vim.diagnostic.config({virtual_text = virtual_text})
+  end,
+  { noremap = true }
+)
+
