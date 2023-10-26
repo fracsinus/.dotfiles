66331e58a89eac545ca2f6800e7d7563d19a9ac5
Ahn Shinho <fracsinus@gmail.com>
2023-10-26 18:20:50 +0900

    fish: Add per-device config

--- config.fish
+++ config.fish
@@ -25,16 +25,7 @@ abbr --add --global -- grhs "git reset --soft"
 abbr --add --global -- gdn "git diff --name-only"
 alias v nvim
 
-
 ## Python
-### pyspark
-# set -x PATH $HOME/.bin $PATH
-# set -x PATH $HOME/glue/bin $HOME/maven/bin $PATH
-# set -x JAVA_HOME /usr/lib/jvm/default
-# set -x SPARK_HOME $HOME/spark
-# set -x SPARK_CONF_DIR $HOME/spark/conf
-# set -x PYTHONPATH $SPARK_HOME/python/:$SPARK_HOME/python/lib/py4j-0.10.9-src.zip:$HOME/glue/PyGlue.zip $PYTHONPATH
-
 ### pyenv init
 if command -v pyenv 1>/dev/null 2>&1
   pyenv init - | source
@@ -52,3 +43,8 @@ fnm env --use-on-cd | source
 
 ## Rust
 set -gx PATH $HOME/.cargo/bin $PATH
+
+## per-device configs
+if [ -f "$(status dirname)/device_config.fish" ]
+  . $(status dirname)/device_config.fish
+end
