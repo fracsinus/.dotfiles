a1004efbf62fdd4eabaa6aa16dbe677be4fb8603
Ahn Shinho <fracsinus@gmail.com>
2024-05-17 12:52:44 +0900

    nvim: Make volar work

--- nvim/lua/lsp/volar.lua
+++ nvim/lua/lsp/volar.lua
@@ -1,10 +1,11 @@
 local base = require("lsp._base")
 local util = require("lspconfig/util")
 
+local global_modules = util.path.join(vim.env.FNM_DIR, "aliases", "default", "lib", "node_modules")
 local function get_typescript_server_path(root_dir)
   -- use typescript install under the project's node_modules
   -- if not found, use global installation
-  local global_ts = util.path.join(vim.env.FNM_DIR, "aliases", "default", "lib", "node_modules", "typescript", "lib")
+  local global_ts = util.path.join(global_modules, "typescript", "lib")
   local found_ts = ""
   local function check_dir(path)
     found_ts =  util.path.join(path, "node_modules", "typescript", "lib")
@@ -20,7 +21,10 @@ local function get_typescript_server_path(root_dir)
 end
 
 require("lspconfig").volar.setup({
-  filetypes = {"typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json"},
+  filetypes = { "typescript", "javascript", "vue" },
+  init_options = {
+    vue = { hybridMode = false },
+  },
   on_new_config = function(new_config, new_root_dir)
     new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
   end,
