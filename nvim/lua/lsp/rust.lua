ba49b86d93499ec4004c8587baa23fc9893e51ff
Ahn Shinho <fracsinus@gmail.com>
2024-06-07 10:38:47 +0900

    nvim: Update LSP settings for Rust

--- nvim/lua/lsp/rust.lua
+++ nvim/lua/lsp/rust.lua
@@ -1,11 +1,17 @@
 local base = require("lsp._base")
 
+local function on_attach(client, bufnr)
+  base.on_attach_lsp(client, bufnr)
+  vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+end
+
 require("lspconfig").rust_analyzer.setup({
   settings = {
     ["rust-analyzer"] = {
+      cargo = { allFeatures = true },
       diagnostics = { enable = true },
     }
   },
-  on_attach = base.on_attach_lsp,
+  on_attach = on_attach,
   capabilities = base.capabilities,
 })
