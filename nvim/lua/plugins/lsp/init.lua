df940ee3effce7020647053e595b5a67e75c0ed3
Ahn Shinho <fracsinus@gmail.com>
2024-06-12 12:01:48 +0900

    nvim: Add goto-preview

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -19,7 +19,26 @@ M = {
         { noremap = true }
       )
     end
-  }
+  },
+  {
+    "rmagatti/goto-preview",
+    dependencies = { "neovim/nvim-lspconfig" },
+    opts = {
+      post_open_hook = function(buf, win)
+        vim.api.nvim_set_option_value("modifiable", false, { buf = buf })
+        vim.keymap.set("n", "q", require("goto-preview").close_all_win, { buffer = buf, noremap = true })
+        vim.keymap.set("n", "<BS>", ":q<CR>", { buffer = buf, noremap = true })
+      end
+    },
+    config = function(_, opts)
+      local p = require("goto-preview")
+      p.setup(opts)
+      vim.keymap.set("n", "gd", p.goto_preview_definition, { noremap = true })
+      vim.keymap.set("n", "gD", p.goto_preview_declaration, { noremap = true })
+      vim.keymap.set("n", "gt", p.goto_preview_type_definition, { noremap = true })
+      vim.keymap.set("n", "gi", p.goto_preview_implementation, { noremap = true })
+    end
+  },
 }
 
 for _, v in ipairs(require("plugins.lsp._cmp")) do
