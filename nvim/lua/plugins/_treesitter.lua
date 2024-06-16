f2cea6f0284a0495a2b65f0f1dcaa6b9bb15d62d
Ahn Shinho <fracsinus@gmail.com>
2024-06-16 16:06:27 +0900

    nvim: Ignore empty lines when comment

--- nvim/lua/plugins/_treesitter.lua
+++ nvim/lua/plugins/_treesitter.lua
@@ -45,7 +45,8 @@ return {
     "numToStr/Comment.nvim",
     dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
     opts = function(_, opts)
-      opts.pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook()
+      opts.ignore = "^$"
+      opts.pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
     end
   },
 }
