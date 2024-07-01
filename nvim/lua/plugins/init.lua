6f3723d313cac3605a51857328da3c0a9bf2a306
Ahn Shinho <fracsinus@gmail.com>
2024-07-01 13:08:20 +0900

    nvim: Remove python indent plugin

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,8 +1,8 @@
 return {
-  {
-    'Vimjas/vim-python-pep8-indent', -- PEP8 compliant indent
-    config = function()
-      vim.g["python_pep8_indent_multiline_string"] = -2
-    end,
-  },
+  -- {
+  --   'Vimjas/vim-python-pep8-indent', -- PEP8 compliant indent
+  --   config = function()
+  --     vim.g["python_pep8_indent_multiline_string"] = -2
+  --   end,
+  -- },
 }
