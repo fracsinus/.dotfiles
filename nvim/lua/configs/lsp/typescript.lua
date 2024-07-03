752cf022d795b479cc4908f0c8fa9e8b9e059d9c
Ahn Shinho <fracsinus@gmail.com>
2024-07-03 15:24:20 +0900

    nvim(lsp): Fix vue/ts configs

--- nvim/lua/configs/lsp/typescript.lua
+++ nvim/lua/configs/lsp/typescript.lua
@@ -1,6 +1,17 @@
 local default = require("configs.lsp._default")
+local util = require("lspconfig/util")
 
 require("lspconfig").tsserver.setup({
+  init_options = {
+    plugins = {
+      {
+        name = "@vue/typescript-plugin",
+        location = util.path.join(vim.env.FNM_DIR, "aliases", "default", "lib", "node_modules", "@vue", "language-server"),
+        languages = { "vue" },
+      },
+    },
+  },
   on_attach = default.on_attach_lsp,
   capabilities = default.capabilities,
+  filetypes = { "typescript", "javascript", "vue" }
 })
--- nvim/lua/configs/lsp/volar.lua
+++ nvim/lua/configs/lsp/volar.lua
@@ -21,10 +21,7 @@ local function get_typescript_server_path(root_dir)
 end
 
 require("lspconfig").volar.setup({
-  filetypes = { "typescript", "javascript", "vue" },
-  init_options = {
-    vue = { hybridMode = false },
-  },
+  filetypes = { "vue" },
   on_new_config = function(new_config, new_root_dir)
     new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
   end,
