ab91930f5ff2d5476438752ff11b56a673f64e89
Ahn Shinho <fracsinus@gmail.com>
2024-11-01 11:34:40 +0900

    nvim: Improve colors

--- nvim/lua/plugins/color.lua
+++ nvim/lua/plugins/color.lua
@@ -9,6 +9,10 @@ return {
       on_highlights = function(hl, _)
         hl.DiagnosticVirtualTextError.bg = nil;
       end,
+      on_colors = function(colors)
+        colors.fg_gutter = "#747a8f"
+        colors.terminal_black = "#6d759c"
+      end,
     },
     config = function (_, opts)
       require("tokyonight").setup(opts)
