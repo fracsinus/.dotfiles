79870f900a19e32b91b18ff2fb297a27013923b4
Ahn Shinho <fracsinus@gmail.com>
2024-11-14 11:28:55 +0900

    nvim: Disable treesitter indent for sql

--- nvim/lua/plugins/_treesitter.lua
+++ nvim/lua/plugins/_treesitter.lua
@@ -27,7 +27,7 @@ return {
         highlight = { enable = true },
         indent = {
           enable = true,
-          disable = { "yaml" },
+          disable = { "yaml", "sql"},
         },
         additional_vim_regex_highlighting = false,
       })
