62ebec1c4bc9ac7995b27250c75bbd5f9eb9ecc9
Ahn Shinho <fracsinus@gmail.com>
2024-12-26 18:15:11 +0900

    nvim: Set `switchbuf`

--- nvim/lua/configs/lsp/_default.lua
+++ nvim/lua/configs/lsp/_default.lua
@@ -34,6 +34,8 @@ M.on_attach_lsp = function(client, bufnr)
   vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
   vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
   vim.keymap.set("n", "<space>f", function() vim.lsp.buf.format { async = true } end, bufopts)
+
+  vim.o.switchbuf = "uselast,usetab,newtab"
 end
 
 return M
