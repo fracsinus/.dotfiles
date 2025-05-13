dd4b896723c69b6d56444a166def55f2db8439bc
Ahn Shinho <fracsinus@gmail.com>
2025-05-13 18:32:07 +0900

    nvim: Use `make` for build

--- nvim/lua/plugins/_telescope.lua
+++ nvim/lua/plugins/_telescope.lua
@@ -5,7 +5,8 @@ return {
       "nvim-lua/plenary.nvim",
       {
         'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cd build && make',
+        -- build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cd build && make',
+        build = 'make',
       },
     },
     config = function(_, _)
