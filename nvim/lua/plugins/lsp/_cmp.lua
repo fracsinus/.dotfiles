74d87ed6c3b6e58a067b0019f798f3ed67509506
Ahn Shinho <fracsinus@gmail.com>
2025-09-25 14:52:27 +0900

    nvim: Add cmp mapping

--- nvim/lua/plugins/lsp/_cmp.lua
+++ nvim/lua/plugins/lsp/_cmp.lua
@@ -38,6 +38,8 @@ return {
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
+          ["<A-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+          ["<A-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
           ["<Tab>"] = cmp.mapping(
             function(fallback)
               if cmp.visible()
